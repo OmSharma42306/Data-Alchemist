fbe4a65c1954cef71af433420f11c43c045477eb
Om Sharma <omsharma.83173@gmail.com>
2025-06-29 01:15:29 +0530

    feat : initialized-project-and-added-ui-components.

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { RecoilRoot } from "recoil"
+
+export default function Home() {
+  return (
+    <RecoilRoot>
+      <div className="min-h-screen bg-background">
+        Hi
+      </div>
+    </RecoilRoot>
+  )
+}
